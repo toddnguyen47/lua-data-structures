13374c185476a9afdc57f32ec03712a7c1ebd151
Todd Nguyen <todd.nguyen47@gmail.com>
2020-03-17 15:29:30 -0700

    Finished adding insert()

--- List.lua
+++ List.lua
@@ -1,5 +1,17 @@
----Structured after Python's lists. Ref: https://docs.python.org/3.8/tutorial/datastructures.html
-local List = {}
+--[[
+  Structured after Python's lists. Ref: https://docs.python.org/3.8/tutorial/datastructures.html
+
+  The implemented functions, in alphabetical order, are:
+  Constructors:
+    new()
+    __init__()
+
+  Functions:
+    append(itemToAppend)
+    clear()
+    get(index)
+    size()
+]] local List = {}
 List.__index = List
 
 function List:new()
@@ -20,17 +32,30 @@ function List:append(itemToAppend)
   table.insert(self._innerTable, itemToAppend)
 end
 
----@return integer size The size of our current list
-function List:size()
-  return self._size
+function List:clear()
+  self._size = 0
+  self._innerTable = {}
 end
 
 ---We will use 0th base index.
 ---@param index integer The index to get
 ---@return any item Item to get at index `index`
 function List:get(index)
-  if (index > self._size) then return nil end
-  return self._innerTable[index + 1]
+  local newIndex = index + 1
+  if (newIndex > self._size) then return nil end
+  return self._innerTable[newIndex]
+end
+
+---@param index integer The desired index, 0th base
+---@param itemToInsert any Any item to insert at desired index
+function List:insert(index, itemToInsert)
+  self._size = self._size + 1
+  table.insert(self._innerTable, index + 1, itemToInsert)
+end
+
+---@return integer size The size of our current list
+function List:size()
+  return self._size
 end
 
 return List
--- List_spec.lua
+++ List_spec.lua
@@ -3,6 +3,10 @@ local List = require("List")
 insulate("List Unit Tests", function()
   local list = List:new()
 
+  before_each(function()
+    list:clear()
+  end)
+
   randomize()
 
   test("Test Append to empty list", function()
@@ -13,5 +17,39 @@ insulate("List Unit Tests", function()
     -- Assert
     assert.is_equal(1, list:size())
     assert.is_equal(itemToInsert, list:get(0))
+  end)
+
+  test("Append 5 items to a list", function()
+    -- Arrange
+    local inputs = {5, 4, "adsf", 1, "hi"}
+    -- Act
+    for i = 1, #inputs do list:append(inputs[i]) end
+    -- Assert
+    for i = 1, #inputs do assert.is_equal(inputs[i], list:get(i - 1)) end
+    assert.is_equal(#inputs, list:size())
+  end)
+
+  local assertInsertAtIndex = function(desiredIndex)
+    -- Arrange
+    local inputs = {5, 4, "adsf", 1, "hi"}
+    for i = 1, #inputs do list:append(inputs[i]) end
+    local newItem = "hello world"
+    -- Act
+    list:insert(desiredIndex, newItem)
+    -- Assert
+    assert.is_equal(#inputs + 1, list:size())
+    assert.is_equal(newItem, list:get(desiredIndex))
+  end
+
+  test("Insert at index 3 (0th) in a 5 item table", function()
+    assertInsertAtIndex(3)
+  end)
+
+  test("Insert at 0th index", function()
+    assertInsertAtIndex(0)
+  end)
+
+  test("Insert at last element", function()
+    assertInsertAtIndex(4)
   end)
 end)
