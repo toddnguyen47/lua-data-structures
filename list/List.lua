b9abb6018a95399952ed549d54a39c790e6ba566
Todd Nguyen <todd.nguyen47@gmail.com>
2020-03-17 17:11:21 -0700

    Minor formatting changes

--- list/List.lua
+++ list/List.lua
@@ -17,69 +17,72 @@
     print()
     remove(itemToRemove)
     size()
-]] local List = {}
+]] --
+local List = {}
 List.__index = List
 
 function List:new()
-  local newInstance = setmetatable({}, self)
-  newInstance:__init__()
+    local newInstance = setmetatable({}, self)
+    newInstance:__init__()
 
-  return newInstance
+    return newInstance
 end
 
 function List:__init__()
-  self._innerTable = {}
-  self._size = 0
+    self._innerTable = {}
+    self._size = 0
 end
 
 ---@param itemToAppend any Item to append to our List
 function List:append(itemToAppend)
-  self._size = self._size + 1
-  table.insert(self._innerTable, itemToAppend)
+    self._size = self._size + 1
+    table.insert(self._innerTable, itemToAppend)
 end
 
 function List:clear()
-  self._size = 0
-  self._innerTable = {}
+    self._size = 0
+    self._innerTable = {}
 end
 
 ---@param itemToFind any Item to find
 ---Count how often `itemToFind` appears in this list
 function List:count(itemToFind)
-  local count = 0
-  for _, v in ipairs(self._innerTable) do if v == itemToFind then count = count + 1 end end
-  return count
+    local count = 0
+    for _, v in ipairs(self._innerTable) do
+        if v == itemToFind then count = count + 1 end
+    end
+    return count
 end
 
 ---We will use 0th base index.
 ---@param index integer The index to get
 ---@return any item Item to get at index `index`
 function List:get(index)
-  local newIndex = index + 1
-  if (newIndex > self._size) then return nil end
-  return self._innerTable[newIndex]
+    local newIndex = index + 1
+    if (newIndex > self._size) then return nil end
+    return self._innerTable[newIndex]
 end
 
 ---@param itemToFind any Item to find
 ---@return integer index Index of `itemToFind`
 ---Find the index of `itemToFind`, returning nil if `itemToFind` cannot be found
 function List:index(itemToFind)
-  local index = nil
-  for k, v in ipairs(self._innerTable) do
-    if v == itemToFind then
-      index = k - 1
-      break
+    local index = nil
+    for k, v in ipairs(self._innerTable) do
+        if v == itemToFind then
+            index = k - 1
+            break
+        end
     end
-  end
-  return index
+    return index
 end
 
 ---@param index integer The desired index, 0th base
 ---@param itemToInsert any Any item to insert at desired index
 ---Insert into the desired index.
 function List:insert(index, itemToInsert)
-  self._size = self._size + 1
-  table.insert(self._innerTable, index + 1, itemToInsert)
+    self._size = self._size + 1
+    table.insert(self._innerTable, index + 1, itemToInsert)
 end
 
 ---From Python's documentation:
@@ -91,34 +94,34 @@ end
 ---@return integer item The item that was popped at `index`, or the last item in the list.
 ---Pop an element at `index`, or the last element if no index is given
 function List:pop(index)
-  local indexToPop = self._size
-  if index ~= nil then indexToPop = index + 1 end
-  if indexToPop > self._size then return nil end
-  self._size = self._size - 1
-  return table.remove(self._innerTable, indexToPop)
+    local indexToPop = self._size
+    if index ~= nil then indexToPop = index + 1 end
+    if indexToPop > self._size then return nil end
+    self._size = self._size - 1
+    return table.remove(self._innerTable, indexToPop)
 end
 
 function List:print()
-  for k, v in ipairs(self._innerTable) do print("[" .. (k - 1) .. "] -> " .. v) end
+    for k, v in ipairs(self._innerTable) do
+        print("[" .. (k - 1) .. "] -> " .. v)
+    end
 end
 
 ---@param itemToRemove any Removing an item that matches `itemToRemove`, otherwise nil
 function List:remove(itemToRemove)
-  local itemRemoved = nil
-  for k, v in ipairs(self._innerTable) do
-    if v == itemToRemove then
-      itemRemoved = v
-      table.remove(self._innerTable, k)
-      self._size = self._size - 1
-      break
+    local itemRemoved = nil
+    for k, v in ipairs(self._innerTable) do
+        if v == itemToRemove then
+            itemRemoved = v
+            table.remove(self._innerTable, k)
+            self._size = self._size - 1
+            break
+        end
     end
-  end
-  return itemRemoved
+    return itemRemoved
 end
 
 ---@return integer size The size of our current list
-function List:size()
-  return self._size
-end
+function List:size() return self._size end
 
 return List
