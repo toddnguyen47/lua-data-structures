04caae94ca18ec74df7bd5066f1007bd70e7b220
Todd Nguyen <todd.nguyen47@gmail.com>
2020-04-03 15:24:14 -0700

    Added a clean flag to our run_and_build script

--- trim/run_and_build.sh
+++ trim/run_and_build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Ref: https://stackoverflow.com/a/7069755
+while test $# -gt 0; do
+  case "$1" in
+    clean)
+      # Ref: https://stackoverflow.com/a/4325357
+      find ./build -type f -not -name '.gitignore' -print0 | xargs -0 rm --
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 mkdir -p build
 cd build
 cmake ../
