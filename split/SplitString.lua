06c35ed945356e6ec0e26c8484e609016597a8a3
Todd Nguyen <todd.nguyen47@gmail.com>
2020-06-23 00:41:14 -0700

    Add lua's splitstring

--- split/SplitString.lua
+++ split/SplitString.lua
@@ -5,7 +5,44 @@ SplitString.__index = SplitString
 
 ---@param strInput string
 ---@param delimiter string
+---@return table
 function SplitString:split(strInput, delimiter)
+  delimiter = delimiter or "\n"
+  local t1 = {}
+  local s1 = ""
+  for char1 in strInput:gmatch(".") do
+    if char1 == delimiter then
+      table.insert(t1, s1)
+      s1 = ""
+    else
+      s1 = s1 .. char1
+    end
+  end
+
+  if s1 ~= "" or next(t1) == nil then table.insert(t1, s1) end
+  return t1
+end
+
+---@param strInput string
+---@param delimiter string
+---@return table
+function SplitString:splitIgnoreEmpty(strInput, delimiter)
+  delimiter = delimiter or "\n"
+  local t1 = {}
+  local s1 = ""
+  for char1 in strInput:gmatch(".") do
+    if char1 == delimiter then
+      if s1 ~= "" then
+        table.insert(t1, s1)
+        s1 = ""
+      end
+    else
+      s1 = s1 .. char1
+    end
+  end
+
+  if s1 ~= "" or next(t1) == nil then table.insert(t1, s1) end
+  return t1
 end
 
 return SplitString
--- split/SplitString_spec.lua
+++ split/SplitString_spec.lua
@@ -2,9 +2,71 @@
 local SplitString = require("SplitString")
 
 describe("Split String | ", function()
-  test("Split on 1 whitespace", function()
-    local a = "Hello World"
-    local t1 = SplitString:split(a, " ")
-    assert.is_same(t1, {"Hello", "World"})
+  describe("Regular Split", function()
+    randomize()
+
+    test("Split on 1 whitespace", function()
+      local a = "Hello World"
+      local t1 = SplitString:split(a, " ")
+      assert.is_same(t1, {"Hello", "World"})
+    end)
+
+    test("Split on 3 whitespace", function()
+      local a = "Hello   World"
+      local t1 = SplitString:split(a, " ")
+      assert.is_same({"Hello", "", "", "World"}, t1)
+    end)
+
+    test("Split on empty string", function()
+      local a = ""
+      local t1 = SplitString:split(a)
+      assert.is_same({""}, t1)
+    end)
+
+    test("Split on newline", function()
+      local a = "hello\n\nworld"
+      local t1 = SplitString:split(a)
+      assert.is_same({"hello", "", "world"}, t1)
+    end)
+
+    test("Split on Pipe", function()
+      local a = "HELLO|HARMFUL|FILTER"
+      local t1 = SplitString:split(a, "|")
+      assert.is_same({"HELLO", "HARMFUL", "FILTER"}, t1)
+    end)
+  end)
+
+  describe("Split ignore empty", function()
+    randomize()
+
+    test("Split on 1 whitespace ignore empty", function()
+      local a = "Hello World"
+      local t1 = SplitString:splitIgnoreEmpty(a, " ")
+      assert.is_same({"Hello", "World"}, t1)
+    end)
+
+    test("Split on 3 whitespace ignore empty", function()
+      local a = "Hello   World"
+      local t1 = SplitString:splitIgnoreEmpty(a, " ")
+      assert.is_same({"Hello", "World"}, t1)
+    end)
+
+    test("Split on empty string ignore empty", function()
+      local a = ""
+      local t1 = SplitString:splitIgnoreEmpty(a)
+      assert.is_same({""}, t1)
+    end)
+
+    test("Split on newline ignore empty", function()
+      local a = "hello\n\nworld"
+      local t1 = SplitString:splitIgnoreEmpty(a)
+      assert.is_same({"hello", "world"}, t1)
+    end)
+
+    test("Split on Pipe", function()
+      local a = "HELLO|HARMFUL|FILTER"
+      local t1 = SplitString:splitIgnoreEmpty(a, "|")
+      assert.is_same({"HELLO", "HARMFUL", "FILTER"}, t1)
+    end)
   end)
 end)
